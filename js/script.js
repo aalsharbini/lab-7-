f7308d5afb35becb1d599495ebad6b38c5bc96b6
aalsharbini <89406314+aalsharbini@users.noreply.github.com>
2022-04-13 02:28:14 -0500

    added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function init(){
+//add your javascript between these two lines of code
+  function alertFunction(){
+	var x1 = document.getElementById("entryinput").value;
+	
+	var x2 = document.getElementById("textoutput");
+	alert("Ala Alsharbini: "+ x1);
+	
+	x1.innerHTML = x1;
+  }
+  var x3 = document.getElementById('entrybutton');
+  
+  x3.addEventListener("click", alertFunction);
+}
+window.addEventListener('load', init);
